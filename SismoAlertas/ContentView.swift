24a69cea1cfa5bf0de75127db6744a6f6ab99ffa
hdevl <lope.hubert2.0@gmail.com>
2026-05-08 14:49:21 -0500

    Initial Commit

---
+++ SismoAlertas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SismoAlertas
+//
+//  Created by huber lope on 8/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SismoAlertas/SismoAlertasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SismoAlertasApp.swift
+//  SismoAlertas
+//
+//  Created by huber lope on 8/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SismoAlertasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
